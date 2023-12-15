f389a9ad8c7671c626690ff6663ca7931dd52a9a
aladdinalizada <tu7c5zneb@code.edu.az>
2023-12-16 03:38:05 +0400

    setup react-helmet

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet";
 import Header from "./layouts/header/header";
 import Hero from "./pages/Hero/Hero";
 import Zero from "./pages/Zero/Zero";
@@ -6,6 +7,13 @@ function App() {
   localStorage.setItem("lang", "az");
   return (
     <>
+      <Helmet>
+        <title>Az2sec</title>
+        <meta name="description" content="Az2sec" />
+        <meta name="keywords" content="Az2sec" />
+        <meta name="author" content="Az2sec" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Helmet>
       <div className="App">
         <Zero />
         <Header />
