36d314e7d95358510d2e3deb9c5e0d8f58465b42
aladdinalizada <tu7c5zneb@code.edu.az>
2023-12-31 22:54:41 +0400

    plans and measures complete

---
+++ src/components/PlansSlider/index.jsx
@@ -0,0 +1,9 @@
+const PLansSlider = () => {
+  return (
+    <div className="w-4/5 h-96 bg-[#F1F5F9] p-7 rounded-xl sm:w-[30%]">
+      <span>AD - Qısa məlumat</span>
+    </div>
+  );
+};
+
+export default PLansSlider;
--- src/pages/PlansAndMeasures/index.jsx
+++ src/pages/PlansAndMeasures/index.jsx
@@ -1,10 +1,16 @@
+import PLansSlider from "../../components/PlansSlider";
+
 const PlansAndMeasures = () => {
   return (
-    <div className="w-full my-8 flex flex-col">
+    <div className="w-full my-8 flex flex-col gap-y-16">
       <h2 className="text-5xl font-bold text-[#090914] text-center ">
         Lahiyə və Tədbirlər
       </h2>
-      <div></div>
+      <div className="w-full h-full flex flex-wrap flex-col justify-center items-center gap-y-3 gap-x-3 sm:flex-nowrap sm:flex-row">
+        <PLansSlider />
+        <PLansSlider />
+        <PLansSlider />
+      </div>
     </div>
   );
 };
